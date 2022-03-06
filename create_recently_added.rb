b15c0aa843f170c2f2244dd5db6b98b2ebe12ca8
Rajan Agaskar <ragaskar@gmail.com>
2022-03-06 14:25:59 +0000

    First pass.
    - Writes playlist files. Mount and data dir are hardcoded.
    - Time span is hard coded (1 week).
    - We must re-open MPD::Client because it does not support finds where
    the argument isn't quoted.

---
+++ create_recently_added.rb
@@ -0,0 +1,48 @@
+require 'mpd_client'
+require 'uri'
+
+module MPD
+  class Client
+
+    def recently_added(since_datetime)
+      write_line("find modified-since #{since_datetime.strftime('%FT%TZ')}")
+      fetch_songs
+    end
+  end
+end
+
+class SongPlaylistDecorator
+  def decorate(song)
+    artist = song["artist"]
+    album = song["album"]
+    file = song["file"]
+    title = song["title"]
+
+    {
+      service: 'mpd',
+      albumart: "/albumart?web=#{artist}/#{album}/extralarge&path=/mnt/#{URI.encode(file)}&icon=fa-tags&metadata=true",
+      title: title,
+      artist: artist,
+      album: album,
+      uri: file,
+    }
+  end
+end
+
+require 'date'
+require 'json'
+client = MPD::Client.new
+client.connect('localhost', '6600')
+start_of_week = Date.today - Date.today.wday
+songs = client.recently_added(start_of_week)
+decorator = SongPlaylistDecorator.new
+playlist_formatted_songs = songs.map do |song|
+  decorator.decorate(song)
+end
+if playlist_formatted_songs.length > 0
+  playlist_string = playlist_formatted_songs.to_json
+  playlist_name = "Recently Added: #{start_of_week.strftime}"
+  File.open("/data/playlist/#{playlist_name}", 'w') do |f|
+    f.write(playlist_string)
+  end
+end
